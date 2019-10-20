1829fd8b1f895618253097c7810afdcc8a87e13c
ShayanYaseen <shayan.yaseen1@gmail.com>
2019-10-20 18:45:44 +0530

    save and player sprite,movement changes

--- 2dxplorer/2dxp.py
+++ 2dxplorer/2dxp.py
@@ -23,16 +23,12 @@ red = (255, 0, 0)
 
 # function to create text with font and color
 # get_rect Returns a new rectangle covering the entire surface
-
-
 def text_objects(text, font):
     textSurface = font.render(text, True, white)
     return textSurface, textSurface.get_rect()
 
 # game intro menu that appears for the first time
 # game is loaded
-
-
 def game_intro():
     intro = True
     while intro:
@@ -49,12 +45,11 @@ def game_intro():
         win.blit(TextSurf, TextRect)
 
         TextSurf, TextRect = text_objects(
-            "Made with pygame library by Shayan 18103033 Rajat 18103025 Saiyam 18103030 "            , smallText
-            )
+            "Made with pygame library by Shayan 18103033 Rajat 18103025 Saiyam 18103030 ", smallText)
         TextRect.center = ((dispw/2), (disph/1.1))
         win.blit(TextSurf, TextRect)
         pygame.display.update()
-        time.sleep(5)
+        time.sleep(1)
         intro = False
 
 
@@ -82,45 +77,80 @@ class knight(object):
         self.up = False
         self.down = False
         self.health = 100
+
+        self.aleft = False
+        self.aright = True
+        self.aup = False
+        self.adown = False
         # defined the sprites update for animation while walking right and left
-        self.spr_walk_r = [pygame.transform.scale(pygame.image.load(
-            './png/Walk ({}).png'.format(x)), (64, 64)) for x in range(1, 11)]
-        self.spr_walk_l = [pygame.transform.flip(
-            x, 1, 0) for x in self.spr_walk_r]
+        self.spr_walk_r = [pygame.transform.scale(pygame.image.load('./player/movement/right/{}.png'.format(x)), (64, 64)) for x in range(1,10)]
+        self.spr_walk_l = [pygame.transform.flip(x, 1, 0) for x in self.spr_walk_r]
+        self.spr_walk_u = [pygame.transform.scale(pygame.image.load('./player/movement/up/{}.png'.format(x)), (64, 64)) for x in range(1, 10)]
+        self.spr_walk_d = [pygame.transform.scale(pygame.image.load('./player/movement/down/{}.png'.format(x)), (64, 64)) for x in range(1, 10)]
+
         # defined the sprites update for animation while staying idle at right and left
-        self.spr_idl_r = [pygame.transform.scale(pygame.image.load(
-            './png/Idle ({}).png'.format(x)), (64, 64)) for x in range(1, 11)]
-        self.spr_idl_l = [pygame.transform.flip(
-            x, 1, 0) for x in self.spr_idl_r]
+        self.spr_idl_r = [pygame.transform.scale(pygame.image.load('./player/movement/right/{}.png'.format(x)), (64, 64)) for x in range(1, 10)]
+        self.spr_idl_l = [pygame.transform.flip(x, 1, 0) for x in self.spr_idl_r]
+        self.spr_idl_u = [pygame.transform.scale(pygame.image.load('./player/movement/up/{}.png'.format(x)), (64, 64)) for x in range(1, 10)]
+        self.spr_idl_d = [pygame.transform.scale(pygame.image.load('./player/movement/down/{}.png'.format(x)), (64, 64)) for x in range(1, 10)]
+
+        #attack
+        self.spr_walk_ar = [pygame.transform.scale(pygame.image.load('./player/attack/right/{}.png'.format(x)), (64, 64)) for x in range(1,6)]
+        self.spr_walk_al = [pygame.transform.scale(pygame.image.load('./player/attack/left/{}.png'.format(x)), (64, 64)) for x in range(1,6)]
+        self.spr_walk_au = [pygame.transform.scale(pygame.image.load('./player/attack/up/{}.png'.format(x)), (64, 64)) for x in range(1, 6)]
+        self.spr_walk_ad = [pygame.transform.scale(pygame.image.load('./player/attack/down/{}.png'.format(x)), (64, 64)) for x in range(1, 6)]
 
     def draw(self, win):
         # called by redraw game window will udate the character sprite
         # based on input key
-        if self.framecount+1 >= 30:
+        if self.framecount+1 >= 9:
             self.framecount = 0
         if self.idle:
             if self.left:
                 win.blit(self.spr_idl_l[self.framecount//3], (self.x, self.y))
-            if self.right:
+            elif self.right:
                 win.blit(self.spr_idl_r[self.framecount//3], (self.x, self.y))
+            elif self.up:
+                win.blit(self.spr_idl_u[self.framecount//3], (self.x, self.y))
+            else:
+                win.blit(self.spr_idl_d[self.framecount//3], (self.x, self.y))
         else:
             if self.left:
                 win.blit(self.spr_walk_l[self.framecount//3], (self.x, self.y))
-            else:
+            elif self.right:
                 win.blit(self.spr_walk_r[self.framecount//3], (self.x, self.y))
+            elif self.up:
+                win.blit(self.spr_walk_u[self.framecount//3], (self.x, self.y))
+            #attack sprites
+            elif self.aright:
+                win.blit(self.spr_walk_ar[self.framecount//3], (self.x, self.y))
+            elif self.aleft:
+                win.blit(self.spr_walk_al[self.framecount//3], (self.x, self.y))
+            elif self.aup:
+                win.blit(self.spr_walk_au[self.framecount//3], (self.x, self.y))
+            elif self.adown:
+                win.blit(self.spr_walk_ad[self.framecount//3], (self.x, self.y))
+            
+            else:
+                win.blit(self.spr_walk_d[self.framecount//3], (self.x, self.y))
         self.framecount += 1
         # Draws the health bar on top right of the screen
         pygame.draw.rect(win, (0, 0, 0), (dispw-82, 6, 74, 12))
         if self.health > 0:
-           pygame.draw.rect(win, (255, 0, 0),
-                            (dispw-80, 7, 70*self.health/100, 10))
+           pygame.draw.rect(win, (255, 0, 0),(dispw-80, 7, 70*self.health/100, 10))
 
     # mapped control call this function with update left,rigth,idle -> after this
     # redraw game window function calls knight.draw function
-    def sprite_update(self, l, r, i):
+    def sprite_update(self, l, r, i, u, d,al,ar,au,ad):
         self.left = l
         self.right = r
         self.idle = i
+        self.up = u
+        self.down = d
+        self.aleft = al
+        self.aright = ar
+        self.aup = au
+        self.adown = ad
 
     def sprite_state(self):
         return (self.left, self.right, self.up, self.down, self.idle)
@@ -132,7 +162,7 @@ class goblin_str(object):
         self.y=y
         self.width=30
         self.height=51
-        self.vel=5
+        self.vel=2
         self.framecount=0
         self.left=False
         self.right=False
@@ -236,39 +266,48 @@ class goblin_str(object):
                elif e1.y+e1.curframe.get_height()+e1.height>p1.y>e1.y:
                   p1.health-=5
 
-# main game loop
 
 
 def game_loop():
     run = True
-    # defined and initialized our player knight with initial x and y
     while run:
         clock.tick(30)  # Games fps
         for event in pygame.event.get():
-            # If we pressed the quit button
             if event.type == pygame.QUIT:
+                #save all the arguments requierd
+                #in a text file
+                f = open("save.txt","w+")
+                f.close()
+                f = open("save.txt","w")
+                f.write(str(p1.x))
+                f.write(" ")
+                f.write(str(p1.y))
                 run = False
 
         # Mapping the controls
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP] and p1.y > p1.vel:
-            # no animation for up therefore no sprite update
             p1.y -= p1.vel
             p1.up = 1
-            p1.idle = 0
+            p1.sprite_update(0, 0, 0, 1, 0, 0, 0, 0, 0)
         if keys[pygame.K_DOWN] and p1.y < disph-p1.height-p1.vel:
-            # no animation for down therefore no sprite update
             p1.y += p1.vel
             p1.down = 1
-            p1.idle = 0
+            p1.sprite_update(0, 0, 0, 0, 1,0, 0, 0, 0)
         if keys[pygame.K_LEFT] and p1.x > p1.vel:
-            # sprite and movement update
             p1.x -= p1.vel
-            p1.sprite_update(1, 0, 0)
+            p1.sprite_update(1, 0, 0, 0, 0,0, 0, 0, 0)
         if keys[pygame.K_RIGHT] and p1.x < dispw-p1.width-p1.vel:
-            # sprite and movement update
             p1.x += p1.vel
-            p1.sprite_update(0, 1, 0)
+            p1.sprite_update(0, 1, 0, 0, 0,0, 0, 0, 0)
+        if keys[pygame.K_z] and p1.right:
+            p1.sprite_update(0,0,0,0,0,0,1,0,0)
+        if keys[pygame.K_z] and p1.left:
+            p1.sprite_update(0, 0, 0, 0, 0, 1, 0, 0, 0)
+        if keys[pygame.K_z] and p1.up:
+            p1.sprite_update(0,0,0,0,0,0,0,1,0) 
+        if keys[pygame.K_z] and p1.down:
+            p1.sprite_update(0, 0, 0, 0, 0, 0, 0, 0, 1)
         if not (keys[pygame.K_UP] or keys[pygame.K_DOWN] or keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
             # if no key is pressed no movement
             p1.idle = True
@@ -278,7 +317,10 @@ def game_loop():
 
 
 # created the knight and time
-p1 = knight(300, 300, 64, 64)
+f = open("save.txt", "r")
+save_pos = f.read()
+save_list = save_pos.split()
+p1 = knight(int(save_list[0]), int(save_list[1]), 64, 64)
 e1=goblin_str(800,800)
 clock = pygame.time.Clock()
 '''
